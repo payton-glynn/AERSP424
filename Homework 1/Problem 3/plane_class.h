0457073d8345c2643149b7d7eff31a8c1956918e
payton-glynn <pjglynn29@gmail.com>
2024-02-09 15:23:26 -0500

    added private double and boolean variables to the class

--- Homework 1/Problem 3/plane_class.h
+++ Homework 1/Problem 3/plane_class.h
@@ -1,1 +1,12 @@
 // plane class header file
+#pragma once			// ensures the header file is only included once during compilation
+
+class plane {			// creating a class named plane
+public:					// public members can be accessed from outside the class
+
+private:				// private members can only be accessed within the class
+	double pos;			
+	double vel;
+	double distance;
+	bool at_SCE;
+};

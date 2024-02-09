b25a07b9801cc24e28db6a118b37725a235d74dc
payton-glynn <pjglynn29@gmail.com>
2024-02-09 16:00:33 -0500

    added string variables and #include <string>

--- Homework 1/Problem 3/plane_class.h
+++ Homework 1/Problem 3/plane_class.h
@@ -1,5 +1,7 @@
 // plane class header file
+#include <string>
 #pragma once			// ensures the header file is only included once during compilation
+using namespace std;	// us this so I do not have to type out 'std::' every time
 
 class plane {			// creating a class named plane
 public:					// public members can be accessed from outside the class
@@ -9,4 +11,6 @@ private:				// private members can only be accessed within the class
 	double vel;
 	double distance;
 	bool at_SCE;
+	string origin;
+	string destination;
 };

4102ce059cbaf12a609768f8f5bc5ed869d1305a
payton-glynn <pjglynn29@gmail.com>
2024-02-09 16:08:29 -0500

    attempted to add conatiner from question 2, got errors, commented out

--- Homework 1/Problem 3/plane_class.h
+++ Homework 1/Problem 3/plane_class.h
@@ -1,5 +1,6 @@
 // plane class header file
 #include <string>
+#include <map>
 #pragma once			// ensures the header file is only included once during compilation
 using namespace std;	// us this so I do not have to type out 'std::' every time
 
@@ -13,4 +14,9 @@ private:				// private members can only be accessed within the class
 	bool at_SCE;
 	string origin;
 	string destination;
+	/*
+	std::map<std::string, std::map<std::string, double>> flight_length;
+	flight_length["SCE"]["PHL"] = 160;			// container from question 2
+	*/
+
 };

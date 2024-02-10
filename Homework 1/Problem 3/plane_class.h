c2689b16adc4e545958643d436e1c043f58973d5
payton-glynn <pjglynn29@gmail.com>
2024-02-09 19:08:03 -0500

    added inputs to constructor file; adjusted comments

--- Homework 1/Problem 3/plane_class.h
+++ Homework 1/Problem 3/plane_class.h
@@ -1,12 +1,12 @@
 // plane class header file
 #include <string>
 #include <map>
-#pragma once			// ensures the header file is only included once during compilation
-using namespace std;	// us this so I do not have to type out 'std::' every time
+#pragma once										// ensures the header file is only included once during compilation
+using namespace std;								// us this so I do not have to type out 'std::' every time
 
-class plane {			// creating a class named plane
+class plane {										// creating a class named plane
 
-private:				// private members can only be accessed within the class
+private:											// not needed because a class is normally private										// private members can only be accessed within the class
 	double pos;			
 	double vel;
 	double distance;
@@ -18,44 +18,49 @@ private:				// private members can only be accessed within the class
 	flight_length["SCE"]["PHL"] = 160;			// container from question 2
 	*/
 
-public:					// public members can be accessed from outside the class
-	plane()				// constructor function
+public:												// public members can be accessed from outside the class
+	plane(const string& from, const string& to)		// constructor function
 	{
-
+			// DOES THE CONSTRUCTOR NEED ANYTHING HERE?
 	}
 
-	~plane()				// destructor function
+	~plane()										// destructor function
 	{				
-		
 	}
 
-	void operate(double dt)				// void function because it is not meant to return anything
+	void operate(double dt)							// void function because it is not meant to return anything
 	{
 	}
 
 	// Get functions
-	double getPos() const {
+	double getPos() const 
+	{
 		return pos;
 	}
 
-	double getVel() const {
+	double getVel() const 
+	{
 		return vel;
 	}
 
-	string getOrigin() const {
+	string getOrigin() const 
+	{
 		return origin;
 	}
 
-	string getDestination() const {
+	string getDestination() const 
+	{
 		return destination;
 	}
 
-	bool isAt_SCE() const {
+	bool getAt_SCE() const 
+	{
 		return at_SCE;
 	}
 
 	// Set function
-	void setVel(double vel)  {
-		// do I need to put anything here?
+	void setVel(double vel)  
+	{
+		// DO I NEED ANYTHING HERE?
 	}
 };

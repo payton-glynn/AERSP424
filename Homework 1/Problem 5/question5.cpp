d7b47529f72d701d6103e1fff9484fdc28b1cec7
payton-glynn <pjglynn29@gmail.com>
2024-02-14 11:51:06 -0500

    Problem 5: instantiated object; set velocity; set timestep

--- Homework 1/Problem 5/question5.cpp
+++ Homework 1/Problem 5/question5.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include "plane_class.h"						// allow access to the plane class in this .cpp file
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	plane obj("SCE","ORD");						// instantiate plane class
+	obj.setVel(410);							// flight speed (miles per hour)
+	double dt = 44;								// time step (seconds)
+	dt = dt / 3600;								// convert time step to hours
+
 	return 0;
 }

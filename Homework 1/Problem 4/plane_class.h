e1b1f472d83565d402d3800941feefb09040ec15
payton-glynn <pjglynn29@gmail.com>
2024-02-14 10:51:19 -0500

    initialized position, velocity, as_SCE varibles

--- Homework 1/Problem 4/plane_class.h
+++ Homework 1/Problem 4/plane_class.h
@@ -3,31 +3,37 @@
 #include <string>
 #include <map>
 #pragma once										// ensures the header file is only included once during compilation
-using namespace std;								// us this so I do not have to type out 'std::' every time
+using namespace std;								// use this so I do not have to type out 'std::' every time
 
 class plane {										// creating a class named plane
 
 private:											// not needed because a class is normally private										// private members can only be accessed within the class
-	double pos;			
+	double pos;
 	double vel;
 	double distance;
 	bool at_SCE;
 	string origin;
 	string destination;
-	
+
 	// container from question 2
 	std::map<std::string, std::map<std::string, double>> flight_length;
-	
+
 
 public:												// public members can be accessed from outside the class
-	plane(const string& from, const string& to)		// constructor function
+	plane(const string& from, const string& to) : origin(from), destination(to), pos(0.0), vel(0.0), at_SCE(0)		// constructor function
 	{
-			// DOES THE CONSTRUCTOR NEED ANYTHING HERE?
-		flight_length["SCE"]["PHL"] = 160;
+		flight_length["SCE"]["PHL"] = 160;			// State College to Philadelphia
+		flight_length["SCE"]["ORD"] = 640;			// State College to Chicago
+		flight_length["SCE"]["EWR"] = 220;			// State College to Newark
+
+		distance = flight_length[from][to];			// sets the value of distance variable to the distance between the airports
+
+		cout << "Plane Created at " << &plane << endl;			// printing out the memory address where the plane object is stored
 	}
 
-	~plane()										// destructor function
-	{				
+	~plane()										// destructor function that prints out "Plane Destroyed"
+	{
+		cout << "Plane Destroyed" << endl;
 	}
 
 	void operate(double dt)							// void function because it is not meant to return anything
@@ -35,33 +41,33 @@ public:												// public members can be accessed from outside the class
 	}
 
 	// Get functions
-	double getPos() const 
+	double getPos() const
 	{
 		return pos;
 	}
 
-	double getVel() const 
+	double getVel() const
 	{
 		return vel;
 	}
 
-	string getOrigin() const 
+	string getOrigin() const
 	{
 		return origin;
 	}
 
-	string getDestination() const 
+	string getDestination() const
 	{
 		return destination;
 	}
 
-	bool getAt_SCE() const 
+	bool getAt_SCE() const
 	{
 		return at_SCE;
 	}
 
 	// Set function
-	void setVel(double vel)  
+	void setVel(double vel)
 	{
 		// DO I NEED ANYTHING HERE?
 	}

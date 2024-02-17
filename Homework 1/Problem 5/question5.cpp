b4551506f4a4d925201f4a7c3777fe3224ee9cc8
payton-glynn <pjglynn29@gmail.com>
2024-02-17 14:19:30 -0500

    made iteration loop; printed out time; need to fix postion output

--- Homework 1/Problem 5/question5.cpp
+++ Homework 1/Problem 5/question5.cpp
@@ -5,10 +5,17 @@
 
 int main()
 {
-	plane obj("SCE","ORD");						// instantiate plane class
+	plane obj("SCE","ORD");						// instantiate plane class to go from state college to chicago
 	obj.setVel(410);							// flight speed (miles per hour)
-	double dt = 44;								// time step (seconds)
+	double dt = 15;								// time step (seconds)
 	dt = dt / 3600;								// convert time step to hours
+	int max_iter = 1500;						// maximum number of iterations
+
+	for (int iter = 1; iter <= max_iter; ++iter)
+	{
+		obj.operate(dt);
+			std::cout << "Time: " << dt*3600*iter << ", " << "Position: " << obj.getPos() << " miles." << std::endl;
+	}
 
 	return 0;
 }

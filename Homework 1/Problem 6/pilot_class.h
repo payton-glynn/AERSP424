37109b60f3bf35de60b6dc21ca072bb9d10917d2
payton-glynn <pjglynn29@gmail.com>
2024-02-17 15:54:02 -0500

    changed name around

--- Homework 1/Problem 6/pilot_class.h
+++ Homework 1/Problem 6/pilot_class.h
@@ -5,7 +5,6 @@
 using namespace std;								// use this so I do not have to type out 'std::' every time
 
 class Pilot {
-	string pilot_name = "John";						// IS THIS HOW TO DO IT OR DO WE NEED TO ACCEPT USER INPUT FOR PILOT NAME
 
 private:
 	string name;
@@ -13,13 +12,13 @@ private:
 public:
 	Pilot()											// pilot contstructor
 	{
-		cout << "The pilot's name is " << pilot_name << ". The memory address is: " << this;
+		cout << "The pilot's name is " << name << ". The memory address is: " << this;
 		cout << ". The pilot is at the gate and ready to board the plane." << endl;
 	}
 
 	~Pilot()										// pilot destructor
 	{
-		cout << "The pilot's name is " << pilot_name << ". The pilot is out of the plane." << endl;
+		cout << "The pilot's name is " << name << ". The pilot is out of the plane." << endl;
 	}
 
 	string getName()								// get function
@@ -28,6 +27,6 @@ public:
 	}
 
 	// POINTER 
-
+	plane* myPlane;
 };
 

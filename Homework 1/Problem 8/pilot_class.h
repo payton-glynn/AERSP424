5e695e97fc0d4b6b2c5492ecc8c72f652add25bf
payton-glynn <pjglynn29@gmail.com>
2024-02-20 23:01:58 -0500

    still working on fixes for problem 8

--- Homework 1/Problem 8/pilot_class.h
+++ Homework 1/Problem 8/pilot_class.h
@@ -1,4 +1,5 @@
 // Pilot class header file
+/*
 #include <iostream>
 #include <string>
 #include "plane_class.h"
@@ -33,3 +34,40 @@ public:
 	//std::unique_ptr<Plane> myPlane = std::make_unique<Plane>();						// pointer named myPlane to an object of plane class
 };
 
+*/
+
+// copied over from question 7
+// Pilot class header file
+#include <iostream>
+#include <string>
+#include "plane_class.h"
+#pragma once										// ensures the header file is only included once during compilation
+using namespace std;								// use this so I do not have to type out 'std::' every time
+
+class Pilot {
+
+private:
+	string name;
+
+public:
+	Pilot(const std::string& pilotName, std::unique_ptr<Plane>& plane) : name(pilotName), myPlane(plane.get())
+	//Pilot(string name)											// pilot contstructor
+	{
+		this->name = name;
+		cout << "The pilot's name is " << name << ". The memory address is: " << this;
+		cout << ". The pilot is at the gate and ready to board the plane." << endl;
+	}
+
+	~Pilot()										// pilot destructor
+	{
+		cout << "The pilot's name is " << name << ". The pilot is out of the plane." << endl;
+	}
+
+	string getName()								// get function
+	{
+		return name;
+	}
+
+
+	std::unique_ptr<Plane> myPlane;					// unique pointer named myPlane to an object of plane class
+};
--- Homework 1/Problem 8/question8.cpp
+++ Homework 1/Problem 8/question8.cpp
@@ -43,7 +43,7 @@ int main() {
 
 	return 0;
 }
-*/
+
 
 #include <iostream>
 #include <string>
@@ -83,6 +83,51 @@ int main() {
 			std::swap(pilot_in_command->myPlane, copilot->myPlane);
 			cout << "Pilot " << pilot_in_command->getName() << " with certificate number " << pilot_in_command.get() << " is in control of a plane: " << pilot_in_command->myPlane->getDestination() << endl;
 			cout << "Pilot " << copilot->getName() << " with certificate number " << copilot.get() << " is in control of a plane: " << copilot->myPlane->getDestination() << endl;
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
+*/
+
+// copied over from question 7
+#include <iostream>
+#include <string>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include "pilot_class.h"
+#include "plane_class.h"
+
+using namespace std;
+
+int main() {
+	std::unique_ptr<Pilot> pilot_in_command = std::make_unique<Pilot>("Alpha");
+	std::unique_ptr<Pilot> copilot = std::make_unique<Pilot>("Bravo");
+
+	Plane plane("SCE", "ORD");
+	plane.setVel(425);
+	double dt = 50 / 3600.0;
+	int max_iter = 1200;
+
+	//pilot_in_command->myPlane = &plane;
+
+	for (int iter = 0; iter <= max_iter; ++iter) {
+		plane.operate(dt);
+
+		if (iter == 0) {
+			cout << "The plane " << &plane << " is at SCE" << endl;
+			cout << "Pilot " << pilot_in_command->getName() << " with certificate number " << pilot_in_command << " is in control of a plane: " << &plane << endl;
+			cout << "Pilot " << copilot->getName() << " with certificate number " << copilot << " is in control of a plane: " << ptr << endl;
+			cout << endl;
+		}
+
+		if (plane.getAt_SCE()) {
+			cout << "The plane " << &plane << " is at SCE" << endl;
+			//swap(&plane, ptr);
+			cout << "Pilot " << pilot_in_command->getName() << " with certificate number " << pilot_in_command << " is in control of a plane: " << &plane << endl;
+			cout << "Pilot " << copilot->getName() << " with certificate number " << copilot << " is in control of a plane: " << ptr << endl;
 			cout << endl;
 		}
 	}

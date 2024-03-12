e2367624f315596cefc5415a317e15f4e4bb2887
payton-glynn <pjglynn29@gmail.com>
2024-03-12 11:10:54 -0400

    finished problem 1.1

--- Homework 2/Problem 1/question1.cpp
+++ Homework 2/Problem 1/question1.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+// abstract base class called Sensor
+class Sensor
+{
+public:
+	virtual void gatherData() = 0;		// virtual functions like this need the =0 at the end
+	virtual void processData() = 0;
+
+	// DO I NEED A DESTRUCTOR??
+};
+
 int main()
 {
 	std::cout << "Hello World" << std::endl;

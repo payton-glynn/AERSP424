81931d2410233991abfee51de6da1ede86c41192
payton-glynn <pjglynn29@gmail.com>
2024-03-13 10:55:12 -0400

    finished problem 1.3

--- Homework 2/Problem 1/question1.cpp
+++ Homework 2/Problem 1/question1.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <memory>
+
+// Question 1.1
 
 // abstract base class called Sensor
 class Sensor
@@ -10,6 +14,7 @@ public:
 	// DO I NEED A CONSTRUCTOR OR DESTRUCTOR??
 };
 
+// Question 1.2
 
 // Velocity class derived from Sensor class
 class Velocity : public Sensor
@@ -18,13 +23,13 @@ public:
 	// implementation of gatherData
 	virtual void gatherData() override
 	{
-		std::cout << "Gathering data from Velocity Sensor" << std::endl;	// DO I NEED TO WRITE THE VELOCITY NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Gathering data from Velocity Sensor" << std::endl;
 	}
 
 	// implementation of processData
 	virtual void processData() override
 	{
-		std::cout << "Processing data from Velocity Sensor" << std::endl;	// DO I NEED TO WRITE THE VELOCITY NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Processing data from Velocity Sensor" << std::endl;
 	}
 };
 
@@ -35,13 +40,13 @@ public:
 	// implementation of gatherData
 	virtual void gatherData() override
 	{
-		std::cout << "Gathering data from Attitude Sensor" << std::endl;	// DO I NEED TO WRITE THE ATTITUDE NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Gathering data from Attitude Sensor" << std::endl;
 	}
 
 	// implementation of processData
 	virtual void processData() override
 	{
-		std::cout << "Processing data from Attitude Sensor" << std::endl;	// DO I NEED TO WRITE THE ATTITUDE NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Processing data from Attitude Sensor" << std::endl;
 	}
 };
 
@@ -52,17 +57,42 @@ public:
 	// implementation of gatherData
 	virtual void gatherData() override
 	{
-		std::cout << "Gathering data from Heading Sensor" << std::endl;	// DO I NEED TO WRITE THE HEADING NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Gathering data from Heading Sensor" << std::endl;
 	}
 
 	// implementation of processData
 	virtual void processData() override
 	{
-		std::cout << "Processing data from Heading Sensor" << std::endl;	// DO I NEED TO WRITE THE HEADING NAME DIFFERENTLY, OR IS IT OKAY HARDCODED?
+		std::cout << "Processing data from Heading Sensor" << std::endl;
 	}
 };
 
+// Question 1.3
 
+class SensorFactory		// using modern style pointers in this class 
+{
+public:
+	static std::unique_ptr<Sensor> createSensor(const std::string& sensorName)	// static createSensor function 
+	{
+		// checks if the sensor name matches any of the three I chose to use
+		if (sensorName == "Velocity")
+		{
+			return std::make_unique<Velocity>();
+		}
+		else if (sensorName == "Attitude")
+		{
+			return std::make_unique<Attitude>();
+		}
+		else if (sensorName == "Heading")
+		{
+			return std::make_unique<Heading>();
+		}
+		else   // returns a null pointer if the sensor name does not match any of the three I chose to use
+		{
+			return nullptr;
+		}
+	}
+};
 
 int main()
 {

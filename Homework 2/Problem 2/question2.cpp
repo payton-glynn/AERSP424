3743287c67e3dd9054a08da9c232e2fb7ea8292c
payton-glynn <pjglynn29@gmail.com>
2024-03-14 11:05:16 -0400

    working through problem 2 logic

--- Homework 2/Problem 2/question2.cpp
+++ Homework 2/Problem 2/question2.cpp
@@ -1,4 +1,19 @@
+// Question 2
 #include <iostream>
+
+// My thoughts on how this problem needs to be solved:
+// Task list of the robots:
+	// robots 1 & 3 gather: 1 second
+	// robots 1 & 3 work: 5 seconds
+	// robots 2 & 4 gather: 1 second
+	// robots 2 & 4 work: 5 seconds
+	// robot 5 gathers: 1 second
+	// robot 5 works: 5 seconds
+		// total time: 18 seconds
+// need to use threading to make these robots complete tasks
+	// give each robot and each tool a number to order them
+	// use the above logic to sort the tasks among the robots
+
 
 void operate()
 {

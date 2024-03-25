d660cdf9a90c600a5d2fea239ce0bf81685111fe
payton-glynn <pjglynn29@gmail.com>
2024-03-25 13:48:46 -0400

    added values for number 4

--- Homework 2/Problem 4/question4.cpp
+++ Homework 2/Problem 4/question4.cpp
@@ -1,5 +1,10 @@
 // Question 4			
-// USE REYNOLD'S NUMBER EQUATION  (rho*v*x)/mu
+// USE REYNOLD'S NUMBER EQUATION  (rho*v*c)/mu
+// Values:
+	// rho: 1.225 kg/m^3
+	// v: will vary the velocity
+	// c: 8 meters (near 747 chord)
+	// mu: 1.825*10^-5 kg/(m*s) dynamic viscosity
 
 #include <iostream>
 
